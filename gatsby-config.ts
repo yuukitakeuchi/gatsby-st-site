ab81e171962e057f5ed577d137c833744aeb3b8f
ytakeuchi <ytakeuchi@bbsystem.co.jp>
2023-04-07 13:34:31 +0900

    add markdown plugins and  test page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,15 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    }
+  ],
 }
 
 export default config
---
+++ src/markdown-pages/page2.md
@@ -0,0 +1,9 @@
+# Page2 document
+
+###
+
+これは2ページ目です。
+
+- 1 coding
+- 2 build 
+- 3 deploy 
